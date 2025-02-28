0ba8df96c45bc33d93341f0b6bf7a4e829a3bb11
Juan Pablo Suarez Ramirez <jpsuarez40@gmail.com>
2025-02-27 21:36:56 -0500

    actu

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
   build: {
@@ -21,9 +22,9 @@ export default defineConfig({
       },
       output: {
         manualChunks: {
-          vendor: ['vue'],
-        },
-      },
-    },
-  },
+          vendor: ['vue']
+        }
+      }
+    }
+  }
 })
