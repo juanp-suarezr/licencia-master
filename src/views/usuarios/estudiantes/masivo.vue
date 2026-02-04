aa67464802cee71460edeb3b070205341848d942
Juan Esteban Londoño Gomez <72523178+Estebanxz11@users.noreply.github.com>
2026-02-04 15:48:16 -0500

    fix: update download file extension from .xls to .xlsx for student template

--- src/views/usuarios/estudiantes/masivo.vue
+++ src/views/usuarios/estudiantes/masivo.vue
@@ -126,7 +126,7 @@ const descargarPlantillaEstudiantes = async () => {
     const url = window.URL.createObjectURL(new Blob([response.data]))
     const link = document.createElement('a')
     link.href = url
-    link.setAttribute('download', 'plantilla_estudiantes.xls')
+    link.setAttribute('download', 'plantilla_estudiantes.xlsx')
     document.body.appendChild(link)
     link.click()
     link.remove()
